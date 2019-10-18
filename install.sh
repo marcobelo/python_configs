ef3766dc4eecc66ab001c5bfa464a3696d95c5d3
barone-dev <marco.barone.belo@gmail.com>
2019-10-18 00:08:27 -0300

    Script to copy settings to project root folder and backup old settings on ".old/"

---
+++ install.sh
@@ -0,0 +1,33 @@
+# Author: Marco Barone Belo
+# Github: https://github.com/barone-dev/
+
+# Allow this file to be executed and run it with:
+# $ chmod +x python_configs/install.sh && ./python_configs/install.sh
+
+timestamp=$(date +%s)
+cd ./python_configs
+rm -rf .git
+test -d "../.old/" || mkdir "../.old/"
+mkdir ../.old/python_configs_$timestamp/
+echo "Python Configs 1.0.0"
+echo "Moving files to your project root folder:"
+for file in $(ls -Ap | grep -v /)
+do
+    test -f "../$file" && mv "../$file" "../.old/python_configs_$timestamp/"
+    cp $file ../
+    echo "File ${file} moved"
+done
+
+echo "Moving folders to your project root folder"
+for directory in $(ls -d */)
+do
+    test -d "../$directory" && cp -rf "../$directory" "../.old/python_configs_$timestamp/"
+    cp -rf $directory ../
+    echo "Directory ${directory} moved"
+done
+
+cd ..
+rm -rf install.sh
+rm -rf python_configs/
+
+echo "Thank you for using my configs, feel free to make a pull request with cool aditions."
